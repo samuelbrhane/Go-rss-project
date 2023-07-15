84a6d2a761c4455594ec5c31138de6d73cc4d308
samuelberhane <1samibrhane@gmail.com>
2023-07-15 09:44:20 +0300

    Add cors and chi for routing.

--- main.go
+++ main.go
@@ -1,16 +1,39 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Hello, world!")
 	godotenv.Load(".env")
 	portString := os.Getenv("PORT")
-	fmt.Println(portString)
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		AllowCredentials: false,
+		ExposedHeaders:   []string{"Link"},
+		MaxAge:           300,
+	}))
+
+	server := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server listening to port %v", portString)
+	err := server.ListenAndServe()
+
+	if err != nil {
+		log.Fatal(err)
+	}
 
 }
